7b59a4f94287452a91e3d1fb11cc001b0dc4d1fe
jkindrix <jkindrix@gmail.com>
2018-08-05 05:40:17 -0500

    Remove comments

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -10,31 +10,23 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            // User instruction
-            Console.WriteLine("How many numbers of the Fibonacci series would you like to print?\n");
+            Console.Write("How many numbers of the Fibonacci series would you like to print?: ");
 
-            // Parse input as a double
             double n = Double.Parse(Console.ReadLine());
 
-            // Clear the console display
             Console.Clear();
 
-            // Print 'n' numbers of the Fibonacci series
-            // (Change to the second argument to 1 to start the series at the number 1 instead of 0)
             Fibonacci(n, 0);
         }
 
-        // This Fibonacci() method will write out 'n' numbers of the Fibonacci series to the console using an iterative method
         public static void Fibonacci(double numbers, double startWith)
         {
-            double firstNumber = 0, // First number in the series
-                secondNumber = 1, // Second number
-                nextNumber; // A placeholder for the result of the previous two numbers
+            double firstNumber = 0,
+                secondNumber = 1,
+                nextNumber;
 
             Console.WriteLine("Here are the requested numbers:\n");
 
-            // Input validation for the second parameter
-            // (Only allow the numbers 0 or 1)
             try
             {
                 switch (startWith)
